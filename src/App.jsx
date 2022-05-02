0a1f453283f8dc56f247d8df23417bc2b9adfc43
mateib47 <m.bucur@student.utwente.nl>
2022-05-02 23:59:17 +0200

    cleanup and setup structure

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './app.scss'
+
+const App = () => {
+  return (
+    <div className='app'>App</div>
+  )
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,22 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { store } from './app/store';
-import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/js/bootstrap.js';
+import ReactDom from 'react-dom';
+import App from './App'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-
-serviceWorker.unregister();
+const root = ReactDom.createRoot(document.querySelector("#root"));
+root.render(<App />)
