a3c626be60e107509914c9fc7b6bc0f09e7e0aa9
mateib47 <m.bucur@student.utwente.nl>
2022-05-04 23:59:21 +0200

    add random quote and color fetching

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,45 @@
-import React from 'react'
-import './app.scss'
-import QuoteBox from './components/QuoteBox'
-import { useState } from 'react'
+import React from "react";
+import "./app.scss";
+import QuoteBox from "./components/QuoteBox";
+import { useState } from "react";
 
 const App = () => {
-    const randomFromInterval = (a,b) => {
-        return Math.floor(Math.random() * (a - b + 1) + a)
-    }
+    const colors = ["#BCECE0","#36EEE0", "#F652A0","#4C5270"];
+  const data = [
+    { quote: "The hardest battle is against yourself.", author: "anonymus" },
+    { quote: "stay hard", author: "david goggins" },
+    {
+      quote:
+        "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+      author: "Nelson Mandela",
+    },
+    {
+      quote: "The way to get started is to quit talking and begin doing.",
+      author: "Walt Disney",
+    },
+    {
+        quote: "Life is what happens when you're busy making other plans.",
+        author: "John Lennon",
+      },
+  ];
+  const [newQuote, setNewQuote] = useState(0);
+  const [color, setColor] = useState(0)
 
-    const [newQuote, setNewQuote] = useState(randomFromInterval(0, data.length));
-    const data = [{quote: "The hardest battle is against yourself.", author: 'anonymus'}, {quote:"stay hard", author: 'david goggins'}];
+  const generateNew = () => {
+    setNewQuote(Math.floor(Math.random() * data.length));
+    setColor(Math.floor(Math.random() * data.length));
+  };
 
-   
   return (
-    <div className='app'>
-        <QuoteBox quote={data[newQuote].quote} author={data[newQuote].author} />
+    <div className="app">
+      <QuoteBox
+        quote={data[newQuote].quote}
+        author={data[newQuote].author}
+        color={colors[color]}
+        setNewQuote={generateNew}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/QuoteBox.jsx
+++ src/components/QuoteBox.jsx
@@ -1,21 +1,26 @@
-import React from 'react'
+import "./quoteBox.scss";
+import { Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
 
-const QuoteBox = ({quote, author}) => {
+const QuoteBox = ({ quote, author, setNewQuote, color }) => {
   return (
-    <div id="quote-box" className='quoteBox'>
-        <div className='textBox'>
-            <h1 it="text">{quote}</h1>
-            <h2 id='author'></h2>
+    <div id="quote-box" className="quoteBox card" style={{backgroundColor: color}}>
+      <div className="cardBody">
+        <div className="textBox row">
+          <h1 id="text" className="cardTitle">{quote}</h1>
+          <h2 id="author" className="cardSubtitle">{author}</h2>
         </div>
-        <div className='left'>
-            <a id="tweet-quote" href=""></a>
+        <div className="left">
+          <a id="tweet-quote" href=""></a>
         </div>
-        <div className='right'>
-            <button>New Quote</button>
+        <div className="right">
+          <button className="btn primary" onClick={() => setNewQuote()}>
+            New Quote
+          </button>
         </div>
-
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default QuoteBox
+export default QuoteBox;
