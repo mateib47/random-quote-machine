55d632cfc42018f1484c69e896839619231eff6b
mateib47 <m.bucur@student.utwente.nl>
2022-05-06 23:59:42 +0200

    add complicated random generator

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,15 @@ import { useState } from "react";
 import Topbar from "./components/Topbar/Topbar";
 
 const App = () => {
-    const colors = ["#BCECE0","#36EEE0", "#F652A0","#4C5270"];
+  const colors = [
+    "#3D3C40",
+    "#575659",
+    "#A6A6A6",
+    "#733321",
+    "#445869",
+    "#042940",
+    "#005C53",
+  ];
   const data = [
     { quote: "The hardest battle is against yourself.", author: "anonymus" },
     { quote: "stay hard", author: "david goggins" },
@@ -19,30 +27,61 @@ const App = () => {
       author: "Walt Disney",
     },
     {
-        quote: "Life is what happens when you're busy making other plans.",
-        author: "John Lennon",
-      },
+      quote: "Life is what happens when you're busy making other plans.",
+      author: "John Lennon",
+    },
   ];
+
+  const shuffle = (arr) => {
+    let i = arr.length, j=0, temp;
+    while(i--){
+      j = Math.floor(Math.random() * (i + 1));
+      temp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = temp;
+    }
+    return arr;
+  }
+
   const [newQuote, setNewQuote] = useState(0);
-  const [color, setColor] = useState(0)
+  const [color, setColor] = useState(0);
+  const [colorArr, setColorArr] = useState(shuffle([...Array(colors.length).keys()]));
+  const [quoteArr, setQuoteArr] = useState(shuffle([...Array(data.length).keys()]));
+
 
   const generateNew = () => {
-    setNewQuote(Math.floor(Math.random() * data.length));
-    setColor(Math.floor(Math.random() * data.length));
+    if(color > colors.length - 1){
+      color = 0;
+      setColorArr(shuffle([...Array(colors.length).keys()]));
+    }else{
+      let newCol = color + 1;
+      setColor(newCol);
+    }
+    if(newQuote > data.length - 1){
+      newQuote = 0;
+      setQuoteArr(shuffle([...Array(data.length).keys()]));
+    }else{
+      let newQuot = newQuote + 1;
+
+      setNewQuote(newQuot++);
+    }
   };
+  
+
+
+
 
   return (
     <div className="app">
-      <Topbar/>
+      <Topbar />
       <div className="section">
         <QuoteBox
-        quote={data[newQuote].quote}
-        author={data[newQuote].author}
-        color={colors[color]}
-        setNewQuote={generateNew}
-      />
+          quote={data[quoteArr[newQuote]].quote}
+          author={data[quoteArr[newQuote]].author}
+          color={colors[colorArr[color]]}
+          setNewQuote={generateNew}
+        />
       </div>
-      
     </div>
   );
 };
